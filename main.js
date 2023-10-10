6db75c676136d99bc4008eddaf2e6f080457cf20
Anna Truong <annatruong321@hotmail.com>
2023-10-10 18:52:15 +0100

    Added Google OAuth window

--- main.js
+++ main.js
@@ -1,26 +1,58 @@
 const { app, BrowserWindow } = require('electron')
+const dotenv = require('dotenv');
+dotenv.config();
+let authWindow;
 
-const createWindow = () => {
+const createMainWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600
   })
-
   win.loadFile('src/index.html')
 }
 
-app.whenReady().then(() => {
-    createWindow()
+
+const createAuthWindow = () => {
+  authWindow = new BrowserWindow({
+    width: 500,
+    height: 700,
+    show: false,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
   
+  const authUrl = `https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}&scope=${process.env.SCOPE}`;
+  authWindow.loadURL(authUrl);
+
+  authWindow.once('ready-to-show', () => {
+    authWindow.show();
+  });
+
+  authWindow.on('closed', () => {
+    authWindow = null;
+  });
+
+  // ... Handle OAuth flow and redirection
+
+  // Other setup and event handling
+}
+
+app.whenReady().then(() => {
+    createMainWindow()
+    // Create authentication window
+    createAuthWindow();
     app.on('activate', () => {
       if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow()
+        createMainWindow()
+        if (authWindow === null) {
+          createAuthWindow();
+        }
       }
     })
   })
   
   app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
       app.quit()
-    }
   })
